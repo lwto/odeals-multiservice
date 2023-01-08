c0666eae5cce62fb83d97366a22f1c812733308d
lwto <linwathanoo227@gmail.com>
2023-01-08 20:52:04 +0630

    faq changes

--- resources/js/components/FAQ/Faq.vue
+++ resources/js/components/FAQ/Faq.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="faq-section">
+  <section class="faq-section padding-top padding-bot">
     <div class="container">
       <div class="row justify-content-center">
         <div class="col-md-8">
